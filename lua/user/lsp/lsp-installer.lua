8ad4f626938e7af3273a7661c69b483590b992b6
Evan Moore <evanm@yelp.com>
2022-05-14 18:13:56 -0700

    formatting autofixes

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -9,13 +9,13 @@ lsp_installer.on_server_ready(function(server)
 	}
 
 	 if server.name == "jsonls" then
-	 	local jsonls_opts = require("user.lsp.settings.jsonls")
-	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+		local jsonls_opts = require("user.lsp.settings.jsonls")
+		opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
 	 end
 
 	 if server.name == "sumneko_lua" then
-	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+		local sumneko_opts = require("user.lsp.settings.sumneko_lua")
+		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
 	 end
 
 	-- This setup() function is exactly the same as lspconfig's setup function.
