d82a243e56e3eee2c264b287b5ad92d54bad2264
Evan Moore <evanm@yelp.com>
2023-05-05 07:17:02 -0700

    Add C-n and C-p to nvim-cmp mappings

--- init.lua
+++ init.lua
@@ -627,12 +627,13 @@ cmp.setup({
     end,
   },
   mapping = cmp.mapping.preset.insert({
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
     ["<Tab>"] = cmp.mapping(function(fallback)
