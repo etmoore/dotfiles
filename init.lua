fc82f1015353470bc01c1c092cafc1969b28c9ce
Evan Moore <evanm@yelp.com>
2023-11-01 19:50:39 -0700

    Add keymaps for disabling/enabling diagnostics

--- init.lua
+++ init.lua
@@ -501,7 +501,9 @@ require("nvim-treesitter.configs").setup({
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set("n", "<leader>d", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+vim.keymap.set("n", "<leader>dd", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+vim.keymap.set("n", "<leader>dh", vim.diagnostic.disable, { desc = "Hide diagnostics" })
+vim.keymap.set("n", "<leader>ds", vim.diagnostic.enable, { desc = "Show diagnostics" })
 
 -- prevent errors appearing in-line
 vim.diagnostic.config({ virtual_text = false })
