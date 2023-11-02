f4b15aee1b6f1407bca06bcc26196f923d2488ff
Evan Moore <evanm@yelp.com>
2023-11-01 19:48:45 -0700

    Disable LSP Formatting

--- init.lua
+++ init.lua
@@ -393,24 +393,25 @@ vim.keymap.set("n", "<leader>sr", require("telescope.builtin").lsp_references, {
 -----------------------------------
 local null_ls = require("null-ls")
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.prettierd,
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-  end,
-})
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- null_ls.setup({
+--   sources = {
+--     null_ls.builtins.formatting.prettierd,
+--     null_ls.builtins.code_actions.eslint,
+--   },
+--   on_attach = function(client, bufnr)
+--     if client.supports_method("textDocument/formatting") then
+--       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--       vim.api.nvim_create_autocmd("BufWritePre", {
+--         group = augroup,
+--         buffer = bufnr,
+--         callback = function()
+--           vim.lsp.buf.format({ bufnr = bufnr })
+--         end,
+--       })
+--     end
+--   end,
+-- })
 
 -----------------------------------
 ---- TREESITTER
