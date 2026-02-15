159f8b95e253e842e391ae452ee87d589d33871e
Evan Moore <evanm@yelp.com>
2026-02-14 20:06:52 -0800

    Fix conform.nvim JavaScript formatter config syntax
    The nested table syntax { { "prettierd", "prettier" } } for
    "run first available" was the old API. Current conform uses
    stop_after_first = true in a flat table instead.

--- init.lua
+++ init.lua
@@ -759,7 +759,7 @@ require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		python = { "isort", "black" },
-		javascript = { { "prettierd", "prettier" } },
+		javascript = { "prettierd", "prettier", stop_after_first = true },
 	},
 	format_on_save = function(bufnr)
 		-- Disable with a global or buffer-local variable
